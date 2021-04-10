519bfb2c125657b08bfbdf062b9ffd609deae7a7
Bazarova Anastasiya <nassi_13@inbox.ru>
2021-04-10 10:57:05 +0300

    header style

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -9,24 +9,38 @@ import Basket from "../basket/Basket";
 const Header = () =>{
   return (
     <div className="Header">
+
    <div className="Header-nav">
-      <a href="http://">Магазины</a>
-      <a href="http://">Акции</a>
-      <a href="http://">Доставка и оплата</a>
-   </div>
-      <img className="Logo" src="#" alt="logo"/>
-      <a href="http://">Москва,  ул. Науки  25</a>
-   <div className="User-nav">
-       <a className="Favorits" href="#s"> 
-      <Favorits/>
-      </a>
-      <a className="Personal" href="#s">
-         <Personal/>
+      <div className="Header-navLink">
+         <a href="http://">Магазины</a>
+         <a href="http://">Акции</a>
+         <a href="http://">Доставка и оплата</a>
+      </div>
+         <img className="Logo" src="#" alt="logo"/>
+         <a href="http://">Москва,  ул. Науки  25</a>
+      <div className="User-nav">
+         <a className="Favorits" href="#s"> 
+         <Favorits/>
          </a>
-      <a className="Basket" href="#s"> 
-      <Basket/>
-      </a>
-   </div>
+         <a className="Personal" href="#s">
+            <Personal/>
+            </a>
+         <a className="Basket" href="#s"> 
+         <Basket/>
+         </a>
+      </div>
+    </div>
+
+    <div className="mainMenu">
+       <a href="http://">Квадроциклы</a>
+       <a href="http://">Катера</a>
+       <a href="http://">Гидроциклы</a>
+       <a href="http://">Лодки</a>
+       <a href="http://">Вездеходы</a>
+       <a href="http://">Снегоходы</a>
+       <a href="http://">Двигатели</a>
+       <a href="http://">Двигатели</a>
+    </div>
     </div>
   );
 }
