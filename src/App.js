da451f586e23a8b8026827d377841566666d3db3
Bazarova Anastasiya <nassi_13@inbox.ru>
2021-04-10 12:53:30 +0300

    fixed header style

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 
 import './App.css';
-
-import Footer from './components/footer/Footer';
 import Header from './components/header/Header';
+import Footer from './components/footer/Footer';
+
 
 
 const App = () => {
