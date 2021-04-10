4f92576d6e9c11303d9a48c72119ebf1d666ff6f
Bazarova Anastasiya <nassi_13@inbox.ru>
2021-04-10 10:40:34 +0300

    add header icons

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,24 +1,31 @@
 /* eslint-disable react/jsx-no-undef */
 
 import './Header.css';
-import Personal from "../personal/Personal"
+import Personal from "../personal/Personal";
+import Favorits from "../favorits/Favorits";
+import Basket from "../basket/Basket";
+
 
 const Header = () =>{
   return (
     <div className="Header">
    <div className="Header-nav">
-      <a href="http://">Магазины</a>;
-      <a href="http://">Акции</a>;
-      <a href="http://">Доставка и оплата</a>;
+      <a href="http://">Магазины</a>
+      <a href="http://">Акции</a>
+      <a href="http://">Доставка и оплата</a>
    </div>
       <img className="Logo" src="#" alt="logo"/>
-      <a href="http://">Москва,  ул. Науки  25</a>;
+      <a href="http://">Москва,  ул. Науки  25</a>
    <div className="User-nav">
-      {/* <Favorites/> */}
+       <a className="Favorits" href="#s"> 
+      <Favorits/>
+      </a>
       <a className="Personal" href="#s">
          <Personal/>
-         </a>;
-      <a className="Basket" href="#s">корзина</a>;
+         </a>
+      <a className="Basket" href="#s"> 
+      <Basket/>
+      </a>
    </div>
     </div>
   );
